28fed2790a84780e7a9bda3bb5a454763e2c5e18
Ryan Riebesehl <149439516+ryanr003@users.noreply.github.com>
2025-12-03 00:21:29 -0600

    Adding wicked.h

---
+++ wicked.h
@@ -0,0 +1,58 @@
+#ifndef WICKED_H
+#define WICKED_H
+
+//============================================================================================================
+// Glinda's Bubble & Wand Ray Tracer - Main utilities and common includes.
+// By: Ryan Riebesehl
+//============================================================================================================
+
+#include <cmath>
+#include <cstdlib>
+#include <iostream>
+#include <limits>
+#include <memory>
+#include <algorithm>
+
+
+
+// C++ Std Usings:
+using std::make_shared;
+using std::shared_ptr;
+
+
+
+
+// Constants:
+const double infinity = std::numeric_limits<double>::infinity();
+const double pi = 3.1415926535897932385;
+
+
+
+
+// Utility Functions:
+inline double degrees_to_radians(double degrees) {
+    return degrees * pi / 180.0;
+}
+
+inline double random_double() {
+    return std::rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max) {
+    return min + (max-min)*random_double();
+}
+
+inline int random_int(int min, int max) {
+    return int(random_double(min, max+1));
+}
+
+
+
+
+// Common Headers:
+#include "vec3.h"
+#include "color.h"
+#include "ray.h"
+#include "interval.h"
+
+#endif
