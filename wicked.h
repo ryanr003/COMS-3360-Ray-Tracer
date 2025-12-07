1b94fce68f4507566946f28ba4961e05140e100b
Ryan Riebesehl <149439516+ryanr003@users.noreply.github.com>
2025-12-06 19:52:41 -0600

    Update wicked.h

--- wicked.h
+++ wicked.h
@@ -2,32 +2,51 @@
 #define WICKED_H
 
 #include <cmath>
-#include <cstdlib>
 #include <iostream>
 #include <limits>
 #include <memory>
+#include <cstdlib>
+#include <random>
 
+// C++ Std Usings:
 using std::make_shared;
 using std::shared_ptr;
 
+
+// Constants:
 const double infinity = std::numeric_limits<double>::infinity();
 const double pi = 3.1415926535897932385;
 
+
+
+// Utility Functions:
 inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
 }
 
+
+
+// REQUIREMENT: Parallelization - random number generation
 inline double random_double() {
-    return std::rand() / (RAND_MAX + 1.0);
+    static thread_local std::uniform_real_distribution<double> distribution(0.0, 1.0);
+    static thread_local std::mt19937 generator;
+    return distribution(generator);
 }
 
 inline double random_double(double min, double max) {
     return min + (max-min)*random_double();
 }
 
-#include "color.h"
-#include "interval.h"
-#include "ray.h"
+inline int random_int(int min, int max) {
+    return int(random_double(min, max+1));
+}
+
+
+
+// Common Headers:
 #include "vec3.h"
+#include "color.h"
+#include "ray.h"
+#include "interval.h"
 
 #endif
